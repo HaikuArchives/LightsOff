2aee42a0ae954b7fb27e8c8d10e15c955c2330a9
Owen <owenca@users.noreply.github.com>
2018-03-10 15:27:31 +0000

    Make random puzzle generation smarter and more efficient

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -1,6 +1,6 @@
 #include "Grid.h"
 
-#include <stdlib.h>
+#include "Random.h"
 
 Grid::Grid(int8 dimension)
 {
@@ -12,7 +12,6 @@ void Grid::SetDimension(int8 dimension)
 	fDimension = dimension;
 	fData.clear();
 	fData.resize(dimension * dimension, 0);
-	fGrid = fData;
 }
 
 
@@ -24,11 +23,11 @@ void Grid::SetDimension(int8 dimension)
 
 void Grid::Random(int8 minMoves)
 {
-	const int8 numButtons = fGrid.size();
+	const int8 numButtons = fData.size();
 
 	// start with an empty grid
 	for (int8 index = 0; index < numButtons; index++)
-		fData[index] = fGrid[index] = 0;
+		fData[index] = 0;
 
 	/*
 	 * Some of the eigenvectors
@@ -71,35 +70,31 @@ void Grid::Random(int8 minMoves)
 	if (minMoves > numButtons)
 		minMoves = numButtons;
 
-	for (int8 count = 0; count < minMoves; count++) {
-		int8 index;
-
-		// next random button not already marked
-		do
-			index = random() % numButtons;
-		while (fGrid[index]);
-
-		fGrid[index] = 1;	// mark the button to be pressed
-	}
-
-	const int8 n = fDimension;
+	int buttonIndices[numButtons];
 
 	for (int8 index = 0; index < numButtons; index++)
-		if (fGrid[index]) {
-			if (index % n)	// not leftmost column
-				fData[index - 1] = !fData[index - 1];	// left neighbor
+		buttonIndices[index] = index;
 
-			if ((index + 1) % n)	// not rightmost column
-				fData[index + 1] = !fData[index + 1];	// right neighbor
+	const int8 begin = ChooseRandom(buttonIndices, numButtons, minMoves);
+	const int8 n = fDimension;
 
-			if (index >= n)	// not top row
-				fData[index - n] = !fData[index - n];	// neighbor above
+	for (int8 i = 0; i < minMoves; i++) {
+		const int8 index = buttonIndices[begin + i];
 
-			if (index < n * (n - 1))	// not bottom row
-				fData[index + n] = !fData[index + n];	// neighbor below
+		if (index % n)	// not leftmost column
+			fData[index - 1] = !fData[index - 1];	// left neighbor
 
-			fData[index] = !fData[index];
-		}
+		if ((index + 1) % n)	// not rightmost column
+			fData[index + 1] = !fData[index + 1];	// right neighbor
+
+		if (index >= n)	// not top row
+			fData[index - n] = !fData[index - n];	// neighbor above
+
+		if (index < n * (n - 1))	// not bottom row
+			fData[index + n] = !fData[index + n];	// neighbor below
+
+		fData[index] = !fData[index];
+	}
 }
 
 bool Grid::ValueAt(int8 x, int8 y)
